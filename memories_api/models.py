fd8b6d6cc2ee9146292a0793cf78023950872e9d
Heather Mielke <heatherlmielke@gmail.com>
2022-03-25 13:12:57 -0500

    edited the post model

--- memories_api/models.py
+++ memories_api/models.py
@@ -4,8 +4,6 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
-    author = models.ForeignKey(
-        'auth.User', related_name='posts', on_delete=models.CASCADE)
     image = models.TextField(null=True, blank=True)
     caption = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
