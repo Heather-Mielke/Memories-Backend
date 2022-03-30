6af00e958900f33f331145de4aa20aafb1d12308
Heather Mielke <heatherlmielke@gmail.com>
2022-03-30 13:21:32 -0500

    edited myposts path

--- memories_api/urls.py
+++ memories_api/urls.py
@@ -10,15 +10,15 @@ urlpatterns = [
     path('api/auth/login', views.LoginAPI.as_view()),
     path('api/auth/user', views.UserAPI.as_view()),
     path('api/auth/logout', knox_views.LogoutView.as_view(), name='knox_logout'),
-    path('api/users/', views.UserList.as_view(), name='user_list'), # api/contacts will be routed to the ContactList view for handling
-    path('api/users/<int:pk>', views.UserDetail.as_view(), name='user_detail'), # api/contacts will be routed to the ContactDetail view for handling
-    path('api/auth/myposts/', views.MyMemoryList.as_view()),
+    path('api/users/', views.UserList.as_view(), name='user_list'),
+    path('api/users/<int:pk>', views.UserDetail.as_view(), name='user_detail'),
+    path('api/myposts/', views.MyMemoryList.as_view()),
     path('api/posts/', views.MemoryList.as_view()),
     path('api/posts/<int:pk>/', views.MemoryDetail.as_view()),
     path('api/post-favorites/', views.PostFavoriteList.as_view()),
     path('api/post-favorites/<int:pk>/', views.PostFavoriteDetail.as_view()),
     path('api/post-comments/', views.PostCommentList.as_view()),
     path('api/post-comments/<int:pk>/', views.PostCommentDetail.as_view())
-   
+
 
 ]
